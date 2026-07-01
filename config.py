34c5960781f44004d7043d2f6ea6d44e3274829b
lwq <linweiqian@139.com>
2026-07-01 16:25:34 +0800

    更新数据库配置

--- config.py
+++ config.py
@@ -11,8 +11,8 @@ class Config:
     # 数据库
     DB_HOST = os.getenv("DB_HOST", "mysql")
     DB_PORT = int(os.getenv("DB_PORT", "3306"))
-    DB_USER = os.getenv("DB_USER", "jwt")
-    DB_PASSWORD = os.getenv("DB_PASSWORD", "jwt123")
+    DB_USER = os.getenv("DB_USER", "root")
+    DB_PASSWORD = os.getenv("DB_PASSWORD", "Aa123,,")
     DB_NAME = os.getenv("DB_NAME", "jingwutang")
     DB_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}?charset=utf8mb4"
 
